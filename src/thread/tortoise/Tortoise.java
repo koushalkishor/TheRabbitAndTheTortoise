046e4052648519d31e12a566448a9bf9a0d45ae3
koushalkishor <koushal.kishor_cs19@gla.ac.in>
2020-10-29 16:45:18 +0530

    add a switch block()

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -4,4 +4,17 @@ public class Tortoise extends Thread {
 
     private static final int MILESTONES = 5;
 
+    public Tortoise() {
+        super("tortoise");
+    }
+
+    @Override
+    public void run() {
+        for (int index = 0; index < MILESTONES; index++) {
+            switch (index) {
+
+            }
+        }
+    }
+
 }
